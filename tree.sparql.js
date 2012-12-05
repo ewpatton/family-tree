0d6cc1b84314df115baeed0bc14884fdfdb507df
Evan W. Patton <ewpatton@gmail.com>
2012-12-04 19:46:58 -0500

    Wrote in query method

--- tree.sparql.js
+++ tree.sparql.js
@@ -1,12 +1,18 @@
 (function() {
   window["Endpoint"] = {
+
     "query": function(query, continuation) {
+      $.getJSON(endpoint, {"query":query,"output":"json"}, continuation);
     },
+
     "getPeople": function(continuation) {
     },
+
     "getMarriages": function(continuation) {
     },
+
     "getOccupations": function(continuation) {
     }
+
   };
 })();
